99faec3d0fce7b1fb9e6093f4e5ee6d001ce3724
fernandasayuri <fernanda.ito@terceiro-sky.com.br>
2023-03-14 20:27:21 -0300

    ajustando funçlões

--- Sistemas Operacionais/atvA2.c
+++ Sistemas Operacionais/atvA2.c
@@ -11,24 +11,7 @@ int file1Open = true; /* Arquivo hipotetico 1 */
 int file2Open = true; /* Arquivo hipotetico 2 */
 int valor1 = 500;
 
-void handler(int sig){ // can be called asynchronously
-  printf("\n Digite 'X' para confirmar terminar o processo!\n");
-  
-  char input;
-  scanf("%s", &input);
-
-  if (input == 'X' || input == 'x'){
-    printf("Terminando processo");
-
-    printf(" O valor1 = %d\n",valor1);
-    file1Open = false; 
-    file2Open = false;
-    exit(-1);
-  }
-  else return;
-}
-
-void handlerKill(int sig){ // can be called asynchronously
+void handlerKill(){ 
   printf("Fechando arquivos. O status do file1Open eh %d e o file2Open eh %d \n", file1Open, file2Open);
   printf(" O valor1 = %d\n",valor1);
 
@@ -37,20 +20,38 @@ void handlerKill(int sig){ // can be called asynchronously
   exit(-1);
 }
 
+void handlerC(){ 
+  printf("\n Digite 'X' para confirmar terminar o processo!\n");
+  
+  char input;
+  scanf("%s", &input);
+
+  if (input == 'X' || input == 'x'){
+    printf("Terminando processo. ");
+
+    handlerKill();
+  }
+  else return;
+}
+
 int main(){
   pid_t pid;
   pid = fork();
 
   if (!pid){
     printf( "Sou o FILHO. %d\n", getpid() );
-    signal(SIGINT, handler);
+    signal(SIGINT, handlerC);
     signal(SIGTERM, handlerKill);
     return 0;
   }
   else if (pid){
     printf( "Sou o PAI. %d\n", getpid() );
     valor1 /= 20;
-    signal(SIGINT, handler);
+
+    //lidando com CTRL+C
+    signal(SIGINT, handlerC);
+
+    //lidando com comando kill
     signal(SIGTERM, handlerKill);
 
     wait(NULL);
